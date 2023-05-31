e306f574109f73e8d66a8fa5a41ef04b0034adaf
Ed Heiser <deepdivered@gmail.com>
2023-05-31 00:42:12 -0700

    Update code.py
    i sourced the mp3 code on this page
    https://learn.adafruit.com/circuitpython-essentials/circuitpython-mp3-audio?gclid=CjwKCAjwvdajBhBEEiwAeMh1Uz-x2iRGlM9foVp4jV5koHB5J4dBPmpb5SktlmdRixIAt2SU-CujzhoC9hIQAvD_BwE
    
    and this one
    
    https://learn.adafruit.com/mp3-circuitpython-lars?view=all
    
    
    
    the issue i am having is after playing sounds a few times i get a error.
    1 /samples/sea_monster_1.mp3
    Traceback (most recent call last):
      File "code.py", line 148, in <module>
    MemoryError: memory allocation failed, allocating 8708 bytes

--- soundboard/NeoTrellis M4/code.py
+++ soundboard/NeoTrellis M4/code.py
@@ -5,17 +5,21 @@
 import time
 import board
 import audioio
-import audiocore
+#  import audiocore
 import audiomixer
 import adafruit_fancyled.adafruit_fancyled as fancy
 import adafruit_trellism4
-from color_names import * # pylint: disable=wildcard-import,unused-wildcard-import
+#  import digitalio
+import audiomp3 
+
+from color_names import *  # pylint: disable=wildcard-import,unused-wildcard-import
 
 
 PLAY_SAMPLES_ON_START = False
 SELECTED_COLOR = WHITE
 SAMPLE_FOLDER = "/samples/"  # the name of the folder containing the samples
 SAMPLES = []
+
 
 # For the intro, pick any number of colors to make a fancy gradient!
 INTRO_SWIRL = [RED, GREEN, BLUE]
@@ -30,25 +34,28 @@ with open("soundboard.txt", "r") as f:
         cleaned = line.strip()
         if len(cleaned) > 0 and cleaned[0] != "#":
             if cleaned == "pass":
-                SAMPLES.append(("does_not_exist.wav", BLACK))
+                SAMPLES.append(("does_not_exist.mp3", BLACK))
             else:
                 f_name, color = cleaned.split(",", 1)
                 SAMPLES.append((f_name.strip(), eval(color.strip())))
-                
-# Play the welcome wav (if its there) and run intro sequence
-# with audioio.AudioOut(board.A1, right_channel=board.A0) as audio: # stereo
-with audioio.AudioOut(board.A1) as audio: # mono
+
+# set led brightness
+trellis.pixels.brightness = 0.1   
+           
+# Play the welcome mp3 (if its there) and run intro sequence
+# with audioio.AudioOut(board.A1) as audio: # mono
+with audioio.AudioOut(board.A1, right_channel=board.A0) as audio:  # stereo
     try:
-        f = open("welcome.wav", "rb")
-        wave = audiocore.WaveFile(f)
-        audio.play(wave)
+        f = open("welcome.mp3", "rb")
+        mp = audiomp3.MP3Decoder(f)
+        audio.play(mp)
         swirl = 0  # we'll swirl through the colors in the gradient
         while audio.playing:
             for i in range(32):
                 palette_index = ((swirl+i) % 32) / 32
                 color = fancy.palette_lookup(INTRO_SWIRL, palette_index)
-                # print(palette_index, fancy.denormalize(color)) display RGB values of swirl
-                trellis.pixels[(i%8, i//8)] = color.pack()
+                # print(palette_index, fancy.denormalize(color)) display RGB of swirl
+                trellis.pixels[(i % 8, i//8)] = color.pack()
             swirl += 1
             time.sleep(0.005)
         f.close()
@@ -57,7 +64,7 @@ with audioio.AudioOut(board.A1) as audio: # mono
         # just hold a moment
         time.sleep(0.5)
     except OSError:
-        # no welcome.wav file 
+        # no welcome.mp3 file 
         pass
 
 # Parse the first file to figure out what format its in
@@ -65,21 +72,21 @@ channel_count = None
 bits_per_sample = None
 sample_rate = None
 with open(SAMPLE_FOLDER+SAMPLES[0][0], "rb") as f:
-    wav = audiocore.WaveFile(f)
-    channel_count = wav.channel_count
-    bits_per_sample = wav.bits_per_sample
-    sample_rate = wav.sample_rate
+    mp3 = audiomp3.MP3Decoder(f)
+    channel_count = mp3.channel_count
+    bits_per_sample = mp3.bits_per_sample
+    sample_rate = mp3.sample_rate
     print("%d channels, %d bits per sample, %d Hz sample rate " %
-          (wav.channel_count, wav.bits_per_sample, wav.sample_rate))
-
+          (mp3.channel_count, mp3.bits_per_sample, mp3.sample_rate))
+    
     # Audio playback object - we'll go with either mono or stereo depending on
     # what we see in the first file
-    if wav.channel_count == 1:
+    if mp3.channel_count == 1:
         audio = audioio.AudioOut(board.A1)
-    elif wav.channel_count == 2:
+    elif mp3.channel_count == 2:
         audio = audioio.AudioOut(board.A1, right_channel=board.A0)
     else:
-        raise RuntimeError("Must be mono or stereo waves!")
+        raise RuntimeError("Must be mono or stereo mp3s!")
 
 mixer = audiomixer.Mixer(voice_count=2, 
     sample_rate=sample_rate, 
@@ -96,24 +103,24 @@ for i, v in enumerate(SAMPLES):
     filename = SAMPLE_FOLDER+v[0]
     try:
         with open(filename, "rb") as f:
-            wav = audiocore.WaveFile(f)
+            mp3 = audiomp3.MP3Decoder(f)
             print(filename,
                   "%d channels, %d bits per sample, %d Hz sample rate " %
-                  (wav.channel_count, wav.bits_per_sample, wav.sample_rate))
-            if wav.channel_count != channel_count:
+                  (mp3.channel_count, mp3.bits_per_sample, mp3.sample_rate))
+            if mp3.channel_count is not channel_count:
                 pass
-            if wav.bits_per_sample != bits_per_sample:
+            if mp3.bits_per_sample is not bits_per_sample:
                 pass
-            if wav.sample_rate != sample_rate:
+            if mp3.sample_rate is not sample_rate:
                 pass
-            trellis.pixels[(i%8, i//8)] = v[1]
+            trellis.pixels[(i % 8, i//8)] = v[1]
             if PLAY_SAMPLES_ON_START:
-                audio.play(wav)
+                audio.play(mp3)
                 while audio.playing:
                     pass
     except OSError:
         # File not found! skip to next
-        if filename != SAMPLE_FOLDER+"does_not_exist.wav":
+        if filename is not SAMPLE_FOLDER+"does_not_exist.mp3":
             print('Not found: ' + filename)
         pass
 
@@ -128,7 +135,7 @@ def stop_playing_sample(playback_details):
 current_press = set()
 current_background = {"voice" : None, "sample_num": None}
 currently_playing = {"voice" : None}
-#last_samplenum = None
+# last_samplenum = None
 while True:
     pressed = set(trellis.pressed_keys)
     just_pressed = pressed - current_press
@@ -138,7 +145,7 @@ while True:
         try:
             filename = SAMPLE_FOLDER+SAMPLES[sample_num][0]
             f = open(filename, "rb")
-            wav = audiocore.WaveFile(f)
+            mp3 = audiomp3.MP3Decoder(f)
             print(sample_num, filename)
             # Check to see if its background music 
             if filename[9:13] == "bgm_":
@@ -149,13 +156,13 @@ while True:
                 # Check to see if it needs to loop
                 will_loop = False
                 file_name = filename.split(".")[0]
-                if file_name[len(file_name) -4:] == "loop":
+                if file_name[len(file_name) - 4:] == "loop":
                     will_loop = True
-                if current_background["voice"] != None:
+                if current_background["voice"] is not None:
                     print("Interrupt")
                     stop_playing_sample(current_background)
                 trellis.pixels[down] = WHITE
-                mixer.play(wav, voice=0, loop=will_loop)
+                mixer.play(mp3, voice=0, loop=will_loop)
                 current_background = {
                     "voice": 0,
                     "neopixel_location": down,
@@ -163,11 +170,11 @@ while True:
                     "sample_num": sample_num,
                     "file": f}
             else:
-                if currently_playing["voice"] != None:
+                if currently_playing["voice"] is not None:
                     print("Interrupt")
                     stop_playing_sample(currently_playing)
                 trellis.pixels[down] = WHITE
-                mixer.play(wav, voice=1, loop=False)
+                mixer.play(mp3, voice=1, loop=False)
                 currently_playing = {
                     "voice": 1,
                     "neopixel_location": down,
@@ -175,13 +182,13 @@ while True:
                     "sample_num": sample_num,
                     "file": f}
         except OSError:
-            pass # File not found! skip to next
+            pass  # File not found! skip to next
 
     # check if any samples are done
     if not mixer.playing:
-        if currently_playing["voice"] != None:
+        if currently_playing["voice"] is not None:
             stop_playing_sample(currently_playing)
-        if current_background["voice"] != None:
+        if current_background["voice"] is not None:
             stop_playing_sample(current_background)
 
     time.sleep(0.01)  # a little delay here helps avoid debounce annoyances
